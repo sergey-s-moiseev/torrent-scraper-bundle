3a40e51d541039391acfd7aefbb2e936bd96d18b
Alex Romanenko <romanenko.alex.v@gmail.com>
2018-05-04 15:11:15 +0300

    fix constants

--- Constant.php
+++ Constant.php
@@ -1,13 +1,15 @@
 <?php
 namespace SergeySMoiseev\TorrentScraperBundle;
 
+use SergeySMoiseev\TorrentScraper\Adapter;
+
 class Constant
 {
-    const EZTV = 'ezTv';
-    const KICKASS = 'kickassTorrents';
-    const THEPIRATEBAY = 'thePirateBay';
-    const TORRENTZ2 = 'torrentz2';
-    const EXTRATORRENT = 'extratorrent';
+    const EZTV = Adapter\EzTvAdapter::ADAPTER_NAME; // 'ezTv';
+    const KICKASS = Adapter\KickassTorrentsAdapter::ADAPTER_NAME; // 'kickassTorrents';
+    const THEPIRATEBAY = Adapter\ThePirateBayAdapter::ADAPTER_NAME; // 'thePirateBay';
+    const TORRENTZ2 = Adapter\Torrentz2Adapter::ADAPTER_NAME; // 'torrentz2';
+    const EXTRATORRENT = Adapter\ExtratorrentAdapter::ADAPTER_NAME; // 'extratorrent';
 
     public static function torrentScrapers()
     {
